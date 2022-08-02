cb8c1df6fd66a7b1d662fd369ee2664632d63486
ggf <a912816369@gmail.com>
2022-08-02 09:05:56 +0800

    Recyclerview 展开收缩实现

--- app/src/main/java/com/alvin/gf_android/ui/ExpandActivity.kt
+++ app/src/main/java/com/alvin/gf_android/ui/ExpandActivity.kt
@@ -3,13 +3,15 @@ package com.alvin.gf_android.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.alvin.gf_android.R
 import com.alvin.gf_android.base.BaseGFActivity
 import com.alvin.gf_android.databinding.ActivityExpandBinding
 import com.alvin.gf_android.databinding.ItemExpandBinding
 import com.alvin.gf_android.model.ExpandEntity
 import com.alvin.gf_android.vm.ExpandViewModel
-import com.alvin.gfad.utils.linear
+import com.alvin.gfad.layout_manager.StickyGridLayoutManager
+import com.alvin.gfad.utils.reuseAdapter
 import com.alvin.gfad.utils.setData
 import com.alvin.gfad.utils.setup
 
@@ -22,7 +24,14 @@ class ExpandActivity :
 
     override fun initView(savedInstanceState: Bundle?) {
         setTitleName("展开收缩")
-        binding.expandList.linear().setup {
+        binding.expandList.layoutManager = StickyGridLayoutManager(this, 2).apply {
+            spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    return if (binding.expandList.reuseAdapter.isSticky(position)) 2 else 1
+                }
+            }
+        }
+        binding.expandList.setup {
             addType<ExpandEntity>(R.layout.item_expand)
             onBind {
                 getBinding<ItemExpandBinding>()?.item = getItem()
